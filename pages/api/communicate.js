a41e969bce0354615c79d310af55cc35fb8763a3
Vasudev-2308 <kingzvasu660@gmail.com>
2022-11-16 22:01:39 +0530

    Deleted commited .env

--- pages/api/communicate.js
+++ pages/api/communicate.js
@@ -1,46 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-require("dotenv").config();
-const e = require("cors");
-const sparkPost = require("sparkpost");
+var nodemailer = require("nodemailer");
 
-async function handleEmail(req, res) {
-  const client = new sparkPost(process.env.API_KEY);
-  const data = JSON.parse(req.body);
-  const { name, phno, email, sub, msg } = data;
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
 
-  client.transmissions.send({
-    content: {
-      from: "connecttovdx.sparkpost.com",
-      subject: "You have an Invitation",
-      html: `<html>
-        <body>
-        <p>Name : ${name}</p>
-        <br/>
-        <p>Phone : ${phno}</p>
-        <br/>
-        <p>Email : ${email}</p>
-        <br/>
-        <p>Subject : ${sub}</p>
-        <br/>
-        <p>Message : ${msg}</p>
-        <br/>
-        </body>
-      </html>`,
-      
-    },
-    recipients:[{
-      address:email
+var mailOptions = {
+  from: "",
+  to: "",
+  subject: "",
+  phone: "",
+  message: "",
+};
 
-    }]
-  }).then(()=>{
-    res.statusCode=200;
-    res.setHeader("Content-Type", "Application-JSON");
-    res.end(JSON.stringify({error: null}))
-  }).catch(()=>{
-    res.statusCode=200;
-    res.setHeader("Content-Type", "Application-JSON");
-    res.end(JSON.stringify({error: null}))
-  });
-}
-
-export default handleEmail;
+transporter.sendMail(mailOptions, function (err, inf) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Email Sent: " + inf.response);
+  }
+});
