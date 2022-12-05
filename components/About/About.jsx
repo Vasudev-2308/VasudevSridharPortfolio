04b82b9a9696bba6695bf54dcabd8d98749bec6c
Vasudev-2308 <kingzvasu660@gmail.com>
2022-12-05 22:08:11 +0530

    Added Content for About Section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,36 +1,64 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
-import { motion } from "framer-motion";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const About = () => {
+  const size = 25;
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16 mt-16">
+    <div
+      id="about"
+      className="w-full md:h-screen p-2 flex items-center py-16 mt-16"
+    >
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 ">
         <div className="col-span-2 mx-3">
           <p className=" uppercase text-[#ecf0f3] text-xl tracking-widest">
             //About_Me
           </p>
           <h1>Who am I ?</h1>
-          <p className="py-4 text-justify  text-md text-[#ecf0f3]">
-            Proident consectetur nostrud amet et. Nisi excepteur ad eiusmod non
-            in do. Excepteur excepteur sunt quis occaecat laboris nostrud elit
-            exercitation culpa ullamco ex dolore. In veniam dolor eiusmod
-            laboris est incididunt qui laboris nisi tempor deserunt proident in.
-            Reprehenderit dolore excepteur nisi elit et cupidatat. Eu
-            adipisicing qui ea ipsum nisi ad proident dolor dolore duis et
-            tempor minim labore. Nulla eiusmod velit fugiat Lorem et
-            reprehenderit mollit.
-          </p>
-          <p className="text-justify text-md text-[#ecf0f3]">
-            Proident consectetur nostrud amet et. Nisi excepteur ad eiusmod non
-            in do. Excepteur excepteur sunt quis occaecat laboris nostrud elit
-            exercitation culpa ullamco ex dolore. In veniam dolor eiusmod
-            laboris est incididunt qui laboris nisi tempor deserunt proident in.
-            Reprehenderit dolore excepteur nisi elit et cupidatat. Eu
-            adipisicing qui ea ipsum nisi ad proident dolor dolore duis et
-            tempor minim labore. Nulla eiusmod velit fugiat Lorem et
-            reprehenderit mollit.
-          </p>
+
+          <div className="my-2 py-2 px-4 items-center">
+            <ul className="list-disc">
+              <li>
+                
+                <p className="my-2 py-2 px-12 text-justify  text-md text-[#ecf0f3]">
+                  A Software Engineer Working in Unisys
+                </p>
+              </li>
+
+              <li>
+                <p className="my-2 py-2  px-12 text-justify  text-md text-[#ecf0f3]">
+                  {"<A Code Enthusiast../>"}
+                </p>
+              </li>
+
+              <li>
+                <p className="my-2 py-2 px-12 text-justify  text-md text-[#ecf0f3]">
+                  Currently Exploring on EDGE COMPUTING,AI & Learning new
+                  Everyday!!
+                </p>
+              </li>
+
+              <li>
+                <p className="my-2 py-2 px-12 text-justify  text-md text-[#ecf0f3]">
+                  I Love building apps, websites and many more.
+                </p>
+              </li>
+
+              <li>
+                <p className="my-2 py-2 px-12 text-justify  text-md text-[#ecf0f3]">
+                  I don't say no to Collaboration, Hackathon, Flutter and Beer
+                  🍺
+                </p>
+              </li>
+
+              <li>
+                <p className="my-2 py-2 px-12 text-justify  text-md text-[#ecf0f3]">
+                Love writing Dart, C++, Java, Python and building fun experiments on type-level
+                </p>
+              </li>
+            </ul>
+          </div>
+
           <p className="py-4 text-justify  text-md text-[#ecf0f3] underline">
             {" "}
             <a href="https://github.com/Vasudev-2308?tab=repositories">
@@ -38,14 +66,14 @@ const About = () => {
             </a>
           </p>
         </div>
-       
-          <div className="hover:animate-pulse skew-y-0">
-            <img
-              className=" m-auto shadow-xl bg-gray-100 shadow-white rounded-full flex justify center items-center p-4 hover:scale-105 ease-in duration-300"
-              src="https://avatars.githubusercontent.com/u/56473514?v=4"
-              alt="Vdx's Image"
-            />
-          </div>
+
+        <div className="hover:animate-pulse skew-y-0">
+          <img
+            className=" m-auto shadow-xl bg-gray-100 shadow-white rounded-full flex justify center items-center p-4 hover:scale-105 ease-in duration-300"
+            src="https://avatars.githubusercontent.com/u/56473514?v=4"
+            alt="Vdx's Image"
+          />
+        </div>
       </div>
     </div>
   );
