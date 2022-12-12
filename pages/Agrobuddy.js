db08414493f6cff9b7799e19ae1b6322036eaafd
Vasudev-2308 <kingzvasu660@gmail.com>
2022-12-12 21:24:43 +0530

    Fixing API Pagination names

--- pages/Agrobuddy.js
+++ pages/Agrobuddy.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import Image from 'next/image'
+import Head from 'next/head';
 import Project from "../Constants/info.json";
 import agroBuddy from "../public/Assets/Project1.png";
 import Link from 'next/link';
 import ProjectPage from '../components/ProjectPage/ProjectPageTemplate';
 const Agrobuddy = () => {
   return (
+    <div>
+      <Head>
+        <title>Agrobuddy</title>
+      </Head>
       <ProjectPage
       imgsrc={agroBuddy}
       title = {Project.Project1.title}
       domain = {Project.Project1.domain}
       desc = {Project.Project1.desc}
     />
+
+    </div>
+      
   )
 }
 
--- pages/Evote.js
+++ pages/Evote.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Project from "../Constants/info.json";
 import evote from "../public/Assets/Project4.png";
-import Link from 'next/link';
+import Head from 'next/head';
 import ProjectPage from '../components/ProjectPage/ProjectPageTemplate';
 import e from 'cors';
 
 const Evote = () => {
   return (
-    <ProjectPage
+    <div>
+      <Head>
+        <title>E-Vote</title>
+      </Head>
+      <ProjectPage
       imgsrc={evote}
       title = {Project.Project4.title}
       domain = {Project.Project4.domain}
       desc = {Project.Project4.desc}
     />
+
+    </div>
+    
   )
 }
 
